9fc1acb4fed76628bbb8a3e62cc3f439d66916c6
Birger Skogeng Pedersen <birgersp@gmail.com>
2017-01-27 14:06:51 +0100

    Now using a buffer

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -37,6 +37,7 @@ function main() {
     var shaderProgram = gl.createProgram();
 
 
+
     // Load shaders
     var loader = new Loader();
     for (var i in SHADER_FILENAMES) {
@@ -74,20 +75,56 @@ function main() {
     // Start
     function start() {
 
-        var a_Position = gl.getAttribLocation(shaderProgram, 'position');
+        var vertices = new Float32Array([
+            -0.5, 0.5,
+            -0.5, -0.5,
+            0.5, 0.5, 
+            0.5, -0.5
+        ]);
 
-        // Pass vertex position to attribute variable
-        gl.vertexAttrib3f(a_Position, -1, 0.0, 0.0);
+        // Create a buffer object
+        var vertexBuffer = gl.createBuffer();
+
+        // Bind the buffer object to target
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+        // Write date into the buffer object
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+        // Obtain attribute location (pointer)
+        var position = gl.getAttribLocation(shaderProgram, 'a_Position');
+
+        // Assign the buffer object to a_Position variable
+        gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
+
+        // Enable the assignment to a_Position variable
+        gl.enableVertexAttribArray(position);
 
         // Specify the color for clearing <canvas>
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearColor(0, 0, 0, 1);
 
         // Clear <canvas>
         gl.clear(gl.COLOR_BUFFER_BIT);
 
-        // Draw a point
-        gl.drawArrays(gl.POINTS, 0, 1);
+        // Draw the rectangle
+        gl.drawArrays(gl.POINTS, 0, 4);
 
+
+
+//        var position = gl.getAttribLocation(shaderProgram, "position");
+//
+//        gl.vertexAttrib4f(position, 0, 0, 0, 1)
+//
+//
+//        // Specify the color for clearing <canvas>
+//        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+//
+//        // Clear <canvas>
+//        gl.clear(gl.COLOR_BUFFER_BIT);
+//
+//        // Draw a point
+//        gl.drawArrays(gl.POINTS, 0, 1);
+//
     }
 }
 
