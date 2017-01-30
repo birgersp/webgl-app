f2e78235dc4f2caf584cc9a3f0891ef5cccf56ee
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-01-30 15:48:28 +0100

    Renamed Matrix4 class file

--- public_html/scripts/Matrix4.js
+++ public_html/scripts/Matrix4.js
@@ -1,33 +1,35 @@
 
 class Matrix4 extends Float32Array {
-    
+
     static xyzRotation(x, y, z) {
-    
+
         var c = Math.cos;
         var s = Math.sin;
-        return new Matrix4([
+        var matrix = new Matrix4();
+        matrix.set([
             c(y) * c(z), c(z) * s(x) * s(y) - c(x) * s(z), s(x) * s(z) + c(x) * c(z) * s(y), 0,
             c(y) * s(z), c(x) * c(z) + s(x) * s(y) * s(z), c(x) * s(y) * s(z) - c(z) * s(x), 0,
             -s(y), c(y) * s(x), c(x) * c(y), 0,
             0, 0, 0, 1
         ]);
+        return matrix;
     }
 
     static identity() {
-        
-        return new Matrix4([
-            1,0,0,0,
-            0,1,0,0,
-            0,0,1,0,
-            0,0,0,1
+
+        var matrix = new Matrix4();
+        matrix.set([
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
         ]);
+        return matrix;
     }
-    
-    constructor(array) {
-        
-        if (array)
-            super(array);
-        else
-            super(16);
+
+    constructor() {
+
+        super(16);
     }
 }
+
