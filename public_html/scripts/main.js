d1871b07b374f88a666ebc57940651b7e0113d19
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-02-04 15:27:28 +0100

    Now using WebGLEngine

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -1,4 +1,4 @@
-const DEPENDENCIES = ["FileLoader"];
+const DEPENDENCIES = ["FileLoader", "WebGLEngine"];
 
 const SHADER_FILENAMES = {
     VSHADER: "shaders/vshader.webgl",
@@ -12,7 +12,7 @@ const SHADER_VARIABLES = {
     MV_MATRIX: "mvMatrix",
     SAMPLER: "sampler",
     COLOR: "color"
-}
+};
 
 function main() {
 
@@ -32,23 +32,14 @@ function main() {
     var canvas = document.createElement("canvas");
     canvas.id = "canvas";
     document.body.appendChild(canvas);
-    var onResize = function() {
+    var onResize = function () {
         canvas.width = canvas.clientWidth;
         canvas.height = canvas.clientHeight;
     };
     onResize();
 
-
-
-    //
-    var gl = canvas.getContext("webgl");
-    var shaderProgram = gl.createProgram();
-
-
-
-    // FileLoader
+    // Create file loader
     var loader = new FileLoader();
-
 
     // Load shaders
     var shaderFiles = {};
@@ -56,122 +47,45 @@ function main() {
         shaderFiles[SHADER_FILENAMES[i]] = loader.addTextFile(SHADER_FILENAMES[i]);
     }
 
-
     // Load texture
     var crateWallImage = loader.addImageFile("binaries/crate.jpg");
 
 
-    // Compile shaders
-    loader.load(function() {
+    // Load files, start app
+    loader.load(function () {
 
-        // Compile vertex shader
-        var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(vertexShader, shaderFiles[SHADER_FILENAMES.VSHADER].text);
-        gl.compileShader(vertexShader);
-        gl.attachShader(shaderProgram, vertexShader);
+        var gl = canvas.getContext("webgl");
+        var engine = new WebGLEngine(gl);
+        engine.initialize(
+                shaderFiles[SHADER_FILENAMES.VSHADER].text,
+                shaderFiles[SHADER_FILENAMES.FSHADER].text
+                );
 
-        // Compile fragment shader
-        var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(fragmentShader, shaderFiles[SHADER_FILENAMES.FSHADER].text);
-        gl.compileShader(fragmentShader);
-        gl.attachShader(shaderProgram, fragmentShader);
-
-        // Link and use shader program
-        gl.linkProgram(shaderProgram);
-        gl.useProgram(shaderProgram);
-
-        start();
-    });
-
-
-
-    // Start
-    function start() {
-
-        var FSIZE = Float32Array.BYTES_PER_ELEMENT;
-        gl.clearColor(1, 1, 1, 1);
-
-        var positionL = gl.getAttribLocation(shaderProgram, SHADER_VARIABLES.POSITION);
-        gl.enableVertexAttribArray(positionL);
-
-        var colorL = gl.getAttribLocation(shaderProgram, SHADER_VARIABLES.COLOR);
-        gl.enableVertexAttribArray(colorL);
-
-
-        var vertices1 = new Float32Array([
+        var vertices1 = [
             0, -1, 0,
             1, -1, 0,
             1, 0, 0
-        ]);
-        var vertexBuffer1 = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer1);
-        gl.bufferData(gl.ARRAY_BUFFER, vertices1, gl.STATIC_DRAW);
+        ];
+        var indices1 = [0, 1, 2];
 
-        var colors1 = new Float32Array([
-            1, 0, 0, 1,
-            0, 1, 0, 1,
-            0, 0, 1, 1
-        ]);
-        var colorBuffer1 = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer1);
-        gl.bufferData(gl.ARRAY_BUFFER, colors1, gl.STATIC_DRAW);
-
-        var indices = new Uint16Array([0, 1, 2]);
-        var indexBuffer1 = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer1);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-
-        var vertices2 = new Float32Array([
-            0, 1, 0, //
-            -1, 1, 0, //
-            -1, 0, 0
-        ]);
-        var vertexBuffer2 = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer2);
-        gl.bufferData(gl.ARRAY_BUFFER, vertices2, gl.STATIC_DRAW);
-
-        var colors2 = new Float32Array([
-            1, 0, 0, 1,
-            0, 1, 0, 1,
-            0, 0, 1, 1
-        ]);
-        var colorBuffer2 = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer2);
-        gl.bufferData(gl.ARRAY_BUFFER, colors2, gl.STATIC_DRAW);
-
-        var indices2 = new Uint16Array([0, 1, 2]);
-        var indexBuffer2 = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer2);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices2, gl.STATIC_DRAW);
-
-
+        var vertices2 = [
+            -1, 1, 0,
+            -1, 0, 0,
+            0, 1, 0
+        ];
+        var indices2 = [0, 1, 2];
 
         gl.clear(gl.COLOR_BUFFER_BIT);
+        
+        var model1 = engine.addModel(vertices1, indices1);
+        var model2 = engine.addModel(vertices2, indices2);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer1);
-        gl.vertexAttribPointer(positionL, 3, gl.FLOAT, false, 0, 0);
+        engine.writeToBuffers();
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer1);
-        gl.vertexAttribPointer(colorL, 4, gl.FLOAT, false, 0, 0);
+        engine.renderModel(model1);
+        engine.renderModel(model2);
 
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer2);
-        gl.drawElements(gl.TRIANGLES, 3, gl.UNSIGNED_SHORT, 0);
-
-
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer2);
-        gl.vertexAttribPointer(positionL, 3, gl.FLOAT, false, 0, 0);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer2);
-        gl.vertexAttribPointer(colorL, 4, gl.FLOAT, false, 0, 0);
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer2);
-        gl.drawElements(gl.TRIANGLES, 3, gl.UNSIGNED_SHORT, 0);
-
-    }
-
-
+    });
 }
 
 require(DEPENDENCIES, main);
