3c6df092e7b69bb7e6d1669e5377f3677220c36e
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-02-06 23:58:26 +0100

    Minor optimization in Transform matrix update

--- public_html/scripts/Transform.js
+++ public_html/scripts/Transform.js
@@ -31,21 +31,29 @@ class Transform {
     }
 
     updateMatrix() {
-
+        
+        var crX = Math.cos(this.r[0]);
+        var crY = Math.cos(this.r[1]);
+        var crZ = Math.cos(this.r[2]);
+        
+        var srX = Math.sin(this.r[0]);
+        var srY = Math.sin(this.r[1]);
+        var srZ = Math.sin(this.r[2]);
+        
         this.m.set([
-            this.s[0] * Math.cos(this.r[1]) * Math.cos(this.r[2]),
-            this.s[0] * (Math.cos(this.r[0]) * Math.sin(this.r[2]) + Math.cos(this.r[2]) * Math.sin(this.r[0]) * Math.sin(this.r[1])),
-            this.s[0] * (Math.sin(this.r[0]) * Math.sin(this.r[2]) - Math.cos(this.r[0]) * Math.cos(this.r[2]) * Math.sin(this.r[1])),
+            this.s[0] * crY * crZ,
+            this.s[0] * (crX * srZ + crZ * srX * srY),
+            this.s[0] * (srX * srZ - crX * crZ * srY),
             0,
 
-            -this.s[1] * Math.cos(this.r[1]) * Math.sin(this.r[2]),
-            this.s[1] * (Math.cos(this.r[0]) * Math.cos(this.r[2]) - Math.sin(this.r[0]) * Math.sin(this.r[1]) * Math.sin(this.r[2])),
-            this.s[1] * (Math.cos(this.r[2]) * Math.sin(this.r[0]) + Math.cos(this.r[0]) * Math.sin(this.r[1]) * Math.sin(this.r[2])),
+            -this.s[1] * crY * srZ,
+            this.s[1] * (crX * crZ - srX * srY * srZ),
+            this.s[1] * (crZ * srX + crX * srY * srZ),
             0,
 
-            this.s[2] * Math.sin(this.r[1]),
-            -this.s[2] * Math.cos(this.r[1]) * Math.sin(this.r[0]),
-            this.s[2] * Math.cos(this.r[0]) * Math.cos(this.r[1]),
+            this.s[2] * srY,
+            -this.s[2] * crY * srX,
+            this.s[2] * crX * crY,
             0,
 
             this.t[0],
