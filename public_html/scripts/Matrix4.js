7722ac1b9625c842fe9562646e6f1bd624a6b442
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-02-06 20:54:16 +0100

    Added matrix multiplication to Matrix4 class

--- public_html/scripts/Matrix4.js
+++ public_html/scripts/Matrix4.js
@@ -32,4 +32,26 @@ class Matrix4 extends Float32Array {
         this[row + column * 4] = value;
         return this;
     }
+
+    multiply(matrix) {
+
+        this.set([
+            this[0] * matrix[0] + this[4] * matrix[1] + this[8] * matrix[2] + this[12] * matrix[3],
+            this[0] * matrix[4] + this[4] * matrix[5] + this[8] * matrix[6] + this[12] * matrix[7],
+            this[0] * matrix[8] + this[4] * matrix[9] + this[8] * matrix[10] + this[12] * matrix[11],
+            this[0] * matrix[12] + this[4] * matrix[13] + this[8] * matrix[14] + this[12] * matrix[15],
+            this[1] * matrix[0] + this[5] * matrix[1] + this[9] * matrix[2] + this[13] * matrix[3],
+            this[1] * matrix[4] + this[5] * matrix[5] + this[9] * matrix[6] + this[13] * matrix[7],
+            this[1] * matrix[8] + this[5] * matrix[9] + this[9] * matrix[10] + this[13] * matrix[11],
+            this[1] * matrix[12] + this[5] * matrix[13] + this[9] * matrix[14] + this[13] * matrix[15],
+            this[2] * matrix[0] + this[6] * matrix[1] + this[10] * matrix[2] + this[14] * matrix[3],
+            this[2] * matrix[4] + this[6] * matrix[5] + this[10] * matrix[6] + this[14] * matrix[7],
+            this[2] * matrix[8] + this[6] * matrix[9] + this[10] * matrix[10] + this[14] * matrix[11],
+            this[2] * matrix[12] + this[6] * matrix[13] + this[10] * matrix[14] + this[14] * matrix[15],
+            this[3] * matrix[0] + this[7] * matrix[1] + this[11] * matrix[2] + this[15] * matrix[3],
+            this[3] * matrix[4] + this[7] * matrix[5] + this[11] * matrix[6] + this[15] * matrix[7],
+            this[3] * matrix[8] + this[7] * matrix[9] + this[11] * matrix[10] + this[15] * matrix[11],
+            this[3] * matrix[12] + this[7] * matrix[13] + this[11] * matrix[14] + this[15] * matrix[15]
+        ]);
+    }
 }
