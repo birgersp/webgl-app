434af887194f323c48f0756ab259296ae8b9f173
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-02-07 19:33:18 +0100

    Added camera movement keyboard control

--- public_html/scripts/ViewController.js
+++ public_html/scripts/ViewController.js
@@ -3,15 +3,20 @@ class ViewController {
     constructor(camera) {
 
         this.enabled = false;
-        this.speed = 1;
-        this.keysDown = [];
-        this.prevMouseX_ = 0;
-        this.prevMouseY_ = 0;
+        this.speed = 0.1;
         this.camera = camera;
+        this.keysDown = {};
 
         var viewController = this;
         document.addEventListener("mousemove", function (evt) {
             viewController.mouseMoved(evt);
+        }, false);
+        document.addEventListener("keydown", function (evt) {
+            if (viewController.enabled)
+                viewController.keysDown[event.key] = true;
+        }, false);
+        document.addEventListener("keyup", function (evt) {
+            viewController.keysDown[event.key] = false;
         }, false);
     }
 
@@ -23,40 +28,57 @@ class ViewController {
     disable() {
 
         this.enabled = false;
+        this.keysDown = {};
     }
 
     update() {
 
         if (this.enabled) {
+            if (this.keysDown.a) {
+                this.camera.t[0] -= this.speed * Math.cos(this.camera.r[1]);
+                this.camera.t[2] += this.speed * Math.sin(this.camera.r[1]);
+                this.camera.updated = false;
+            } else
+            if (this.keysDown.d) {
+                this.camera.t[0] += this.speed * Math.cos(this.camera.r[1]);
+                this.camera.t[2] -= this.speed * Math.sin(this.camera.r[1]);
+                this.camera.updated = false;
+            }
 
-
+            if (this.keysDown.w) {
+                this.camera.t[2] -= this.speed * Math.cos(this.camera.r[1]);
+                this.camera.t[1] += this.speed * Math.sin(this.camera.r[0]);
+                this.camera.t[0] -= this.speed * Math.sin(this.camera.r[1]);
+                this.camera.updated = false;
+            } else
+            if (this.keysDown.s) {
+                this.camera.t[2] += this.speed * Math.cos(this.camera.r[1]);
+                this.camera.t[1] -= this.speed * Math.sin(this.camera.r[0]);
+                this.camera.t[0] += this.speed * Math.sin(this.camera.r[1]);
+                this.camera.updated = false;
+            }
         }
     }
 
     mouseMoved(event) {
         if (this.enabled) {
-            this.camera.r[0] -= event.movementY / 1000;
+            this.camera.r[0] -= event.movementY / 2000;
             if (this.camera.r[0] > Math.PI / 2) {
                 this.camera.r[0] = Math.PI / 2;
             } else
             if (this.camera.r[0] < -Math.PI / 2) {
                 this.camera.r[0] = -Math.PI / 2;
             }
-            this.camera.r[1] -= event.movementX / 1000;
+
+            if (this.camera.r[1] > Math.PI) {
+                this.camera.r[1] -= Math.PI * 2;
+            } else
+            if (this.camera.r[1] < -Math.PI) {
+                this.camera.r[1] += Math.PI * 2;
+            }
+
+            this.camera.r[1] -= event.movementX / 2000;
             this.camera.updated = false;
-        }
-    }
-
-    keyDown(event) {
-
-
-    }
-
-    keyUp(event) {
-
-        if (this.enabled) {
-
-
         }
     }
 }
