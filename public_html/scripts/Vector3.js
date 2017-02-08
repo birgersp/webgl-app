891fc265650cdaf2155990a812dc5a06db0aefda
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-02-08 08:17:57 +0100

    Added features to Vector3

--- public_html/scripts/Vector3.js
+++ public_html/scripts/Vector3.js
@@ -11,10 +11,14 @@ class Vector3 extends Float32Array {
     applyMatrix(matrix) {
 
         var result = [
-            matrix[0] * this[0] + matrix[1] * this[1] + matrix[2] * this[2] + matrix[3],
-            matrix[4] * this[0] + matrix[5] * this[1] + matrix[6] * this[2] + matrix[7],
-            matrix[8] * this[0] + matrix[9] * this[1] + matrix[10] * this[2] + matrix[11]
+            matrix[0] * this[0] + matrix[4] * this[1] + matrix[8] * this[2] + matrix[12],
+            matrix[1] * this[0] + matrix[5] * this[1] + matrix[9] * this[2] + matrix[13],
+            matrix[2] * this[0] + matrix[6] * this[1] + matrix[10] * this[2] + matrix[14]
         ];
         this.set(result);
+    }
+
+    scale(factor) {
+        this.set([this[0] * factor, this[1] * factor, this[2] * factor]);
     }
 }
