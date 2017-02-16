394360f3402d2de7783540deee1acdc6ebcf4b91
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-02-16 19:17:29 +0100

    User position set to start higher

--- web/scripts/App.js
+++ web/scripts/App.js
@@ -15,7 +15,7 @@ class App {
 
         this.grassTexture = null;
         this.user = new User();
-        this.user.position = new Vector3(0, 30, 0);
+        this.user.position = new Vector3(0, 50, 0);
         this.userOnGround = false;
 //        this.controller.rotation = new Vector3(-Math.PI / 2, 0, 0);
 //        this.controller.mode = Controller.moveMode.FREE;
