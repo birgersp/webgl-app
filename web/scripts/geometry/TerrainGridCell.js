a3f0500a5b4c0a952b075a12a0e6534df0ce8282
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-02-16 20:12:53 +0100

    Implemented improved terrain collision detection

--- web/scripts/geometry/TerrainGridCell.js
+++ web/scripts/geometry/TerrainGridCell.js
@@ -6,25 +6,19 @@ class TerrainGridCell {
         this.topright = topright;
         this.bottomleft = bottomleft;
         this.bottomright = bottomright;
-
-        this.xySlopeTop = (topright[1] - topleft[1]) / (topright[0] - topleft[0]);
-        this.zySlopeLeft = (topleft[1] - bottomleft[1]) / (topleft[2] - bottomleft[2]);
-        this.xySlopeBottom = (bottomright[1] - bottomleft[1]) / (bottomright[0] - bottomleft[0]);
-        this.zySlopeRight = (topright[1] - bottomright[1]) / (topright[2] - bottomright[2]);
     }
 
     getY(x, z) {
 
         let dX = x - this.bottomleft[0];
         let dZ = z - this.bottomleft[2];
-        let dY = 0;
 
-        if (dX < dZ) {
-            dY = dX * this.xySlopeBottom + dZ * this.zySlopeRight;
+        let y = 0;
+        if (dZ - dX > 0) {
+            y = this.bottomleft[1] - dX * this.topleft[1] + dX * this.topright[1] - this.bottomleft[1] * dZ + this.topleft[1] * dZ;
         } else {
-            dY = dX * this.xySlopeTop + dZ * this.zySlopeLeft;
+            y = this.bottomleft[1] - dX * this.bottomleft[1] + dX * this.bottomright[1] - this.bottomright[1] * dZ + this.topright[1] * dZ;
         }
-        return this.bottomleft[1] + dY;
+        return y;
     }
 }
-;
